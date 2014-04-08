46c78c5f4a6244a9a2c9cadee7fb4506d4bb687d
Shmaliy <shmaliy.maxim@proxistep.com>
2014-04-08 16:47:22 +0300

    sdasd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
 var app = {
 
+    devWidth : 0,
+    devHeight: 0,
 
+    displayAjust: function () {
+        app.devHeight = $('html').height();
+        app.devWidth = $('html').width();
+        var barHeight = $('.bar').height();
+
+        var iframeHeight = app.devHeight - barHeight;
+
+        $('#webView').css({
+                width: app.devWidth + 'px',
+                height: iframeHeight + 'px'
+        });
+
+        $('.slide-menu').css({
+            width: app.devWidth + 'px',
+            height: app.devHeight - barHeight + 'px',
+            left: -1 * app.devWidth + 'px',
+            top: barHeight + 'px'
+        });
+
+//        console.log(width);
+    },
+
+    menuToggle: function () {
+        $('#menuToggler').click(function(e) {
+            e.preventDefault();
+            var currentPosition = parseInt($('.slide-menu').css("left"));
+
+            if (currentPosition < 0) {
+
+                $('.slide-menu').animate({
+                    left: "+=" + app.devWidth + 'px'
+                }, 800);
+            } else {
+
+                $('.slide-menu').animate({
+                    left: "-=" + app.devWidth + 'px'
+                }, 800);
+            }
+        });
+    },
 
 
     findByName: function() {
@@ -19,8 +61,12 @@ var app = {
     initialize: function() {
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        app.displayAjust();
+        app.menuToggle();
     }
 
 };
 
-app.initialize();
+$(function(){
+    app.initialize();
+});
