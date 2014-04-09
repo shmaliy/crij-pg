0e8c69c1edf7b1b6118283f89ad5ac30e01d4705
Shmaliy <shmaliy.maxim@proxistep.com>
2014-04-09 18:15:06 +0300

    animate 400 ms

--- www/js/main.js
+++ www/js/main.js
@@ -34,12 +34,12 @@ var app = {
 
                 $('.slide-menu').animate({
                     left: "+=" + app.devWidth + 'px'
-                }, 300);
+                }, 400);
             } else {
 
                 $('.slide-menu').animate({
                     left: "-=" + app.devWidth + 'px'
-                }, 300);
+                }, 400);
             }
         });
     },
